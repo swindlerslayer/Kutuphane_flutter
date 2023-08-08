394ec941b9c7d127b7e103fb45e2073b5c9283e2
deret <privetberkant@gmail.com>
2023-08-08 12:10:15 +0300

    kitapliste getirme

--- lib/Controllers/Degiskenler/Kitap.dart
+++ lib/Controllers/Degiskenler/Kitap.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:convert';
 
 Kitap KitapFromJson(String str) => Kitap.fromJson(json.decode(str));
@@ -16,13 +14,9 @@ class Kitap {
   int? barkod;
   String? kayitYapan;
   String? kayitTarihi;
-  dynamic degisiklikYapan;
+  String? degisiklikYapan;
   String? degisiklikTarihi;
   String? resim;
-  dynamic kitapTuru;
-  dynamic yayinEvi;
-  dynamic yazar;
-  List<dynamic>? kitapOgrenci;
 
   Kitap({
     this.id,
@@ -37,10 +31,6 @@ class Kitap {
     this.degisiklikYapan,
     this.degisiklikTarihi,
     this.resim,
-    this.kitapTuru,
-    this.yayinEvi,
-    this.yazar,
-    this.kitapOgrenci,
   });
 
   factory Kitap.fromJson(Map<String, dynamic> json) => Kitap(
@@ -56,10 +46,6 @@ class Kitap {
         degisiklikYapan: json["DegisiklikYapan"],
         degisiklikTarihi: json["DegisiklikTarihi"],
         resim: json["Resim"],
-        kitapTuru: json["KitapTuru"],
-        yayinEvi: json["YayinEvi"],
-        yazar: json["Yazar"],
-        kitapOgrenci: List<dynamic>.from(json["KitapOgrenci"].map((x) => x)),
       );
 
   Map<String, dynamic> toJson() => {
@@ -75,9 +61,47 @@ class Kitap {
         "DegisiklikYapan": degisiklikYapan,
         "DegisiklikTarihi": degisiklikTarihi,
         "Resim": resim,
-        "KitapTuru": kitapTuru,
-        "YayinEvi": yayinEvi,
-        "Yazar": yazar,
-        "KitapOgrenci": List<dynamic>.from(kitapOgrenci!.map((x) => x)),
+      };
+}
+
+// To parse this JSON data, do
+//
+//     final listeKitap = listeKitapFromJson(jsonString);
+
+List<ListeKitap> listeKitapFromJson(String str) =>
+    List<ListeKitap>.from(json.decode(str).map((x) => ListeKitap.fromJson(x)));
+
+String listeKitapToJson(List<ListeKitap> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class ListeKitap {
+  int? id;
+  String? adi;
+  String? yazarAdi;
+  int? yazarId;
+  String? resim;
+
+  ListeKitap({
+    this.id,
+    this.adi,
+    this.yazarAdi,
+    this.yazarId,
+    this.resim,
+  });
+
+  factory ListeKitap.fromJson(Map<String, dynamic> json) => ListeKitap(
+        id: json["ID"],
+        adi: json["Adi"],
+        yazarAdi: json["YazarAdi"],
+        yazarId: json["YazarID"],
+        resim: json["Resim"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "ID": id,
+        "Adi": adi,
+        "YazarAdi": yazarAdi,
+        "YazarID": yazarId,
+        "Resim": resim,
       };
 }
--- lib/Controllers/kitap_controller.dart
+++ lib/Controllers/kitap_controller.dart
@@ -1,15 +1,12 @@
 import 'package:http/http.dart' as http;
 import 'package:kutuphane_mobil_d/Controllers/Degiskenler/kitap.dart';
-import 'dart:convert';
 import 'package:kutuphane_mobil_d/URL/url.dart';
 
-Future<Kitap?> GetKitap(String KullaniciAdi, String Parola) async {
+Future<List<ListeKitap>?> GetKitap(String KullaniciAdi, String Parola) async {
   var apilink = ApiEndPoints.baseUrl;
   var token = await TokenService.getToken(
       kullaniciAdi: KullaniciAdi, parola: Parola, loginMi: false);
   try {
-    // Get the token using await to wait for the response
-
     final response = await http.get(
       Uri.parse('$apilink/api/kitaplisteyeekle'),
       headers: {
@@ -20,16 +17,13 @@ Future<Kitap?> GetKitap(String KullaniciAdi, String Parola) async {
     );
 
     if (response.statusCode == 200) {
-      // API'den dönen cevabı JSON olarak çözüyoruz.
-      final Map<String, dynamic> responseData = json.decode(response.body);
-
+      // final Map<String, dynamic> responseData = json.decode(response.body);
       // Kitap bilgilerini kullanarak Kitap nesnesini oluşturuyoruz.
-      Kitap kitap = Kitap.fromJson(responseData);
 
       // Kitap dönüyoruz.,
       print('Giriş başarılı ${response.statusCode}');
 
-      return kitap;
+      return listeKitapFromJson(response.body);
     } else {
       // Giriş başarısız oldu, null değeri dönüyoruz.
       print('Giriş başarısız ${response.statusCode}   ');
@@ -43,45 +37,3 @@ Future<Kitap?> GetKitap(String KullaniciAdi, String Parola) async {
     return null;
   }
 }
-
-// Future<Kitap?> GetKitap(String KullaniciAdi, String Parola) async {
-//   var apilink = ApiEndPoints.baseUrl;
-
-//   var token = TokenService.getToken(
-//        kullaniciAdi: KullaniciAdi, parola: Parola, loginMi: false);
-
-
-
-//   try {
-//     final response = await http.get(
-//       Uri.parse('$apilink/api/kitaplisteyeekle'),
-//       headers: {
-//         "Content-Type": "application/x-www-form-urlencoded",
-//         "Authorization": "Bearer $token"
-//       },
-//     );
-
-//     if (response.statusCode == 200) {
-//       // API'den dönen cevabı JSON olarak çözüyoruz.
-//       final Map<String, dynamic> responseData = json.decode(response.body);
-
-//       // Kitap bilgilerini kullanarak Kitap nesnesini oluşturuyoruz.
-//       Kitap kitap = Kitap.fromJson(responseData);
-
-//       // Kitap dönüyoruz.,
-//       print('Giriş başarılı ${response.statusCode}');
-
-//       return kitap;
-//     } else {
-//       // Giriş başarısız oldu, null değeri dönüyoruz.
-//       print('Giriş başarısız ${response.statusCode}   ');
-
-//       return null;
-//     }
-//   } catch (e) {
-//     // Hata oluştuğunda veya API'ye ulaşılamadığında null dönüyoruz.
-//     print('??? ?? $e  ');
-
-//     return null;
-//   }
-// }
