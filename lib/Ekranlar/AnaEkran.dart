bcd6364c2430536337892475c4b4eab93c261b81
deret <privetberkant@gmail.com>
2023-08-09 14:55:25 +0300

    liste örneği 2

--- lib/Ekranlar/AnaEkran.dart
+++ lib/Ekranlar/AnaEkran.dart
@@ -3,28 +3,58 @@ import 'package:kutuphane_mobil_d/Ekranlar/nav-drawer.dart';
 
 class NewScreen extends StatelessWidget {
   final kullanici;
-
   const NewScreen({Key? key, this.kullanici}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     print('Kullanıcı Adı: ${kullanici?.kullaniciAdi}');
-
-    return Scaffold(
-        drawer: NavDrawer(kullanici: kullanici),
-        appBar: AppBar(
-          title: const Text('Ana Ekran'),
+    PopupMenuButton(
+      icon: const Icon(Icons.settings),
+      itemBuilder: (context) => [
+        const PopupMenuItem(
+          child: Text("Settings"),
         ),
-        body: ListView.builder(
-          itemCount: 10,
-          prototypeItem: ListTile(
-            title: Text(kullanici.toString()),
-          ),
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(kullanici.kullaniciAdi.toString()),
-            );
-          },
-        ));
+        const PopupMenuItem(
+          child: Text("Flutter.io"),
+        ),
+        const PopupMenuItem(
+          child: Text("Google.com"),
+        ),
+      ],
+    );
+    return Scaffold(
+      drawer: NavDrawer(kullanici: kullanici),
+      appBar: AppBar(
+        title: const Text('Ana Ekran'),
+      ),
+      body: ListView.builder(
+        shrinkWrap: true,
+        itemCount: 10,
+        prototypeItem: ListTile(
+          title: Text(kullanici.toString()),
+        ),
+        itemBuilder: (context, index) {
+          return const ListTile(
+            title: Text('Three-line ListTile'),
+            subtitle:
+                Text('A sufficiently long subtitle warrants three lines.'),
+            //trailing liste öğesinin sağına yerleştireceğimiz öğeler için...
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+
+            // leading: const Icon(Icons.person),
+            leading: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('leading Line 1'),
+                Text('leading Line 2'),
+              ],
+            ),
+            // title: Text(kullanici.kullaniciAdi.toString()),
+            // textColor: const Color.fromARGB(255, 94, 94, 94)
+          );
+        },
+      ),
+    );
   }
 }
